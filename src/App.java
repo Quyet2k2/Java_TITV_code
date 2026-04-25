6b130658c111237b9ebfbdb7d7dbe2258cc27ec8
Phạm Văn Quyết <quyet051222002@gmail.com>
2026-04-25 17:16:14 +0700

    TITV - Java 65. Hiểu rõ cấu trúc Map của Java | Viết chương trình tra từ điển Anh - Việt

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
   public static void main(String[] args) throws Exception {
     Scanner sc = new Scanner(System.in);
-    RutThamTrungThuong_HashSet hashSet = new RutThamTrungThuong_HashSet(sc);
+    TuDien hashSet = new TuDien(sc);
     hashSet.menu();
   }
 }
