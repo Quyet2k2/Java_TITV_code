966b4c450cd38e4f2f96d806762f482ded923434
Phạm Văn Quyết <quyet051222002@gmail.com>
2026-05-04 08:52:37 +0700

    TITV - Java 67. Cách lập trình tạo tập tin và thư mục trong Java

--- src/App.java
+++ src/App.java
@@ -1,15 +1,27 @@
+import java.io.File;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    // Không thể truyền kiểu dữ liệu khác được.
-    Box box = new Box(15);
-    System.out.println("Gia tri: " + box.getValue());
+    File folder1 = new File("E:\\JAVA_WORKSPACE\\Java_TITV_code\\src\\folder1");
+    File folder2 = new File("E:\\JAVA_WORKSPACE\\Java_TITV_code\\src\\folder2");
+    System.out.println("Kiểm tra folder1 có tồn tại hay không: " + folder1.exists());
+    System.out.println("Kiểm tra folder2 có tồn tại hay không: " + folder2.exists());
 
-    // Có thể truyền kiểu dữ liệu khác được.
-    Box2<Double> box2 = new Box2<Double>(15.5);
-    System.out.println("Gia tri: " + box2.getValue());
+    File d1 = new File("E:\\JAVA_WORKSPACE\\Java_TITV_code\\src\\directory_1");
+    // d1.mkdir(); // không tạo được nhiều thư mục
 
-    Box2<String> box2_2 = new Box2<String>("Muoi lam");
-    System.out.println("Gia tri: " + box2_2.getValue());
+    File d2 = new File("E:\\JAVA_WORKSPACE\\Java_TITV_code\\src\\directory_1\\d2\\d3\\d4");
+    d2.mkdirs(); // tạo được nhiều thư mục
+
+    // Tạo tệp tin (có phần mở rộng)
+    File f1 = new File("E:\\JAVA_WORKSPACE\\Java_TITV_code\\src\\directory_1\\vidu.txt");
+    try {
+      f1.createNewFile();
+    } catch (Exception e) {
+      // Không có quyền tạo tệp tin
+      // Ổ cứng bị đầy
+      // Đường dẫn sai
+      e.printStackTrace();
+    }
   }
 }
