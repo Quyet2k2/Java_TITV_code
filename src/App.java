22d60ac3816256636e7e12ca89559b992a752e01
Phạm Văn Quyết <quyet051222002@gmail.com>
2026-05-09 16:42:59 +0700

    Java 79. Các thành phần và bố cục của chương trình giao diện | Lập trình Java

--- src/App.java
+++ src/App.java
@@ -1,23 +1,44 @@
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class App {
   public static void main(String[] args) throws Exception {
     MyWindow m1 = new MyWindow();
     m1.showIt();
-
-    MyWindow m2 = new MyWindow();
-    m2.showIt("Window 2");
-
-    MyWindow m3 = new MyWindow();
-    m3.showIt("Window 3", 600, 800);
   }
 }
 
 class MyWindow extends JFrame {
   public MyWindow() {
     this.setSize(600, 400);
-    this.setLocation(300, 300);
+    this.setLocationRelativeTo(null);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    // FlowLayout
+    // this.setLayout(new FlowLayout());
+    // this.setLayout(new FlowLayout(FlowLayout.RIGHT));
+    // this.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 50));
+
+    // GridLayout
+    // this.setLayout(new GridLayout());
+    // this.setLayout(new GridLayout(4, 4));
+    // this.setLayout(new GridLayout(4, 4, 25, 25));
+
+    // BorderLayout
+    String[] locationLayout = new String[] {
+        BorderLayout.EAST, BorderLayout.WEST, BorderLayout.SOUTH, BorderLayout.NORTH, BorderLayout.CENTER };
+    this.setLayout(new BorderLayout());
+    this.setLayout(new BorderLayout(15, 15));
+
+    for (int i = 1; i <= 6; i++) {
+      JButton jButton = new JButton(i + "");
+      // this.add(jButton);
+
+      this.add(jButton, locationLayout[i <= locationLayout.length ? i - 1 : 0]);
+    }
   }
 
   public void showIt() {
